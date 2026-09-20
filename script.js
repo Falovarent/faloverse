277a6eebbe8cf77fca08235c46a4648ab3bed769
Falovarent <venomf99@gmail.com>
2026-09-20 14:36:42 +0400

    Simplify cursor movement logic in script.js
    Refactored cursor movement logic to simplify event handling and removed unnecessary variables and animation function.

--- script.js
+++ script.js
@@ -1,49 +1,10 @@
 const cursor = document.querySelector(".cursor");
 
-let mouseX = null;
-let mouseY = null;
-
-let currentX = null;
-let currentY = null;
-
-
-/* =========================
-   RED CURSOR
-========================= */
-
 document.addEventListener("mousemove", function (event) {
-
-    mouseX = event.clientX;
-    mouseY = event.clientY;
-
-    if (currentX === null) {
-        currentX = mouseX;
-        currentY = mouseY;
-
-        cursor.style.opacity = "1";
-    }
-
+    cursor.style.left = event.clientX + "px";
+    cursor.style.top = event.clientY + "px";
+    cursor.style.opacity = "1";
 });
-
-
-function animateCursor() {
-
-    if (mouseX !== null && currentX !== null) {
-
-        currentX += (mouseX - currentX) * 0.18;
-        currentY += (mouseY - currentY) * 0.18;
-
-        cursor.style.left = currentX + "px";
-        cursor.style.top = currentY + "px";
-
-    }
-
-    requestAnimationFrame(animateCursor);
-}
-
-
-animateCursor();
-
 
 /* =========================
    PAGE NAVIGATION
